b13dd0df30cda9f8a3f000b7805a81154bc0b7a2
Rima Jean <rima.jean@gmail.com>
2018-03-01 22:45:09 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,13 @@
 class Book
-  def initialize(title, author, page_count, genre)
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
     @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 end
